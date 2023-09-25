5c9ee0389d98ad083fdc87bda09072ab95fc9aea
Yator1 <titusyatz17@gmail.com>
2023-09-25 09:07:38 -0700

    custom printf file

--- _printf.c
+++ _printf.c
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
         }
         else if (format[i + 1] == '%')
         {
-            _putchar('%');
+            _putchara('%');
         }
         counter++;
     }
